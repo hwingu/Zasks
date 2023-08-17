d1ddacbd6c64011aeedaea38dc0a1260dbe4d4e0
Wing Hong <scargaming233@gmail.com>
2023-08-17 14:42:44 +0800

    Update TagsScrollArea.tsx

--- src/components/TagsScrollArea.tsx
+++ src/components/TagsScrollArea.tsx
@@ -11,7 +11,7 @@ const TagsScrollArea = async (props: Props) => {
   return (
     <ScrollArea className="h-[200px] w-[250px] rounded-md p-2">
       {props.tags.map((tag : any) => (
-        <div className="flex flex-col align-top">
+        <div className="flex flex-col align-top" key={tag.id}>
           {<TagOptions tag={tag} task={props.task} />}
         </div>
       ))}
